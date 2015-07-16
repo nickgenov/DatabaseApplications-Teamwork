6bbb6a8a82b6aadb9d34b5d2075be5d633114ceb
nickgenov <nickolay.r.genov@gmail.com>
2015-07-17 00:07:11 +0300

    Insert table data.

---
+++ Oracle/InsertCategories.sql
@@ -0,0 +1,8 @@
+INSERT INTO CATEGORIES (NAME, DESCRIPTION) VALUES('Beverages', 'Soft drinks, coffees, teas, beers, and ales');
+INSERT INTO CATEGORIES (NAME, DESCRIPTION) VALUES('Condiments', 'Sweet and savory sauces, relishes, spreads, and se');
+INSERT INTO CATEGORIES (NAME, DESCRIPTION) VALUES('Confections', 'Desserts, candies, and sweet breads');
+INSERT INTO CATEGORIES (NAME, DESCRIPTION) VALUES('Dairy Products', 'Cheeses');
+INSERT INTO CATEGORIES (NAME, DESCRIPTION) VALUES('Grains/Cereals', 'Breads, crackers, pasta, and cereal');
+INSERT INTO CATEGORIES (NAME, DESCRIPTION) VALUES('Meat/Poultry', 'Prepared meats');
+INSERT INTO CATEGORIES (NAME, DESCRIPTION) VALUES('Produce', 'Dried fruit and bean curd');
+INSERT INTO CATEGORIES (NAME, DESCRIPTION) VALUES('Seafood', 'Seaweed and fish');
