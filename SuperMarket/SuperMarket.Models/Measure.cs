4ff81211b6318c5937cf29a6a661684e30b17e51
cherrySU <terry@mail.bg>
2015-07-25 17:01:39 +0300

    Supermarket Models

---
+++ SuperMarket/SuperMarket.Models/Measure.cs
@@ -0,0 +1,8 @@
+﻿namespace SuperMarket.Models
+{
+    public class Measure
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ SuperMarket/SuperMarket.Models/Supplier.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperMarket.Models
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
