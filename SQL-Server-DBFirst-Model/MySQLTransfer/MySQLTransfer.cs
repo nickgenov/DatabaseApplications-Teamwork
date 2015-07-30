c21018517be8d1539a98ae5f205743ec7711265d
cherrySU <terry@mail.bg>
2015-07-30 15:02:07 +0300

    MsSQL --> MySQL

--- SQL-Server-DBFirst-Model/MySQLTransfer/MySQLTransfer.cs
+++ SQL-Server-DBFirst-Model/MySQLTransfer/MySQLTransfer.cs
@@ -15,8 +15,7 @@ namespace DataToMySQL
 
     public class ToMySqlTransfer
     {
-
-        public string ExportDataIntoMySql()
+        static void Main()
         {
             var context = new SupermarketContext();
             var measures = context.Measures;
@@ -26,29 +25,93 @@ namespace DataToMySQL
             var products = context.Products;
             var orders = context.Orders;
 
-            const string mySqlDataSource = @"server=localhost;database=supermarket;userid=root;";
-            var connection = new MySqlConnection(mySqlDataSource);
-            connection.Open();
-            using (connection)
+            String mySqlDataSource = @"server=localhost;database=supermarket;userid=root;";
+            MySqlConnection connection = null;
+            try
             {
-                InsertMeasuresIntoDb(measures, connection);
+                connection = new MySqlConnection(mySqlDataSource);
+                connection.Open();
 
-                InsertCategoriesIntoDb(categories, connection);
+                //InsertMeasuresIntoDb(measures, connection);
 
-                InsertCustomersIntoDb(customers, connection);
+                //InsertCategoriesIntoDb(categories, connection);
 
-                InsertSuppliersIntoDb(suppliers, connection);
+                //InsertCustomersIntoDb(customers, connection);
 
-                InsertProductsIntoDb(products, connection);
+                //InsertSuppliersIntoDb(suppliers, connection);
 
-                InsertOrdersIntoDb(orders, connection);
+                //InsertProductsIntoDb(products, connection);
+
+                //InsertOrdersIntoDb(orders, connection);
+
+
+                //String cmdText = "INSERT INTO measures(name) VALUES(@name)";
+                //MySqlCommand cmd2 = new MySqlCommand(cmdText, connection);
+
+                //cmd2.Prepare();
+                //cmd2.Parameters.AddWithValue("@name", "345");
+                //cmd2.ExecuteNonQuery();
+
+
+                foreach (var measure in measures)
+                {
+                    Console.WriteLine(measure.Name);
+                    var insertQuery = "INSERT INTO measures (name) VALUES (@name)";
+                    MySqlCommand cmd = new MySqlCommand(insertQuery, connection);
+                    cmd.Prepare();
+                    cmd.Parameters.AddWithValue("@name", measure.Name);
+                    cmd.ExecuteNonQuery();
+                }
+
+
 
             }
 
-            return "Data exporting into MySql: Done!";
+            catch (MySqlException err)
+            {
+                Console.WriteLine("Error: " + err.ToString());
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close(); //close the connection
+                }
+            }
         }
+        //public string ExportDataIntoMySql()
+        //{
+        //    var context = new SupermarketContext();
+        //    var measures = context.Measures;
+        //    var categories = context.Categories;
+        //    var customers = context.Customers;
+        //    var suppliers = context.Suppliers;
+        //    var products = context.Products;
+        //    var orders = context.Orders;
 
-        private void InsertOrdersIntoDb(IDbSet<Order> orders, MySqlConnection connection)
+        //    const string mySqlDataSource = @"server=localhost;database=supermarket;userid=root;";
+        //    var connection = new MySqlConnection(mySqlDataSource);
+        //    connection.Open();
+        //    using (connection)
+        //    {
+        //        InsertMeasuresIntoDb(measures, connection);
+
+        //        InsertCategoriesIntoDb(categories, connection);
+
+        //        InsertCustomersIntoDb(customers, connection);
+
+        //        InsertSuppliersIntoDb(suppliers, connection);
+
+        //        InsertProductsIntoDb(products, connection);
+
+        //        InsertOrdersIntoDb(orders, connection);
+
+        //    }
+
+        //    return "Data exporting into MySql: Done!";
+        //}
+
+        private static void InsertOrdersIntoDb(IDbSet<Order> orders, MySqlConnection connection)
         {
             foreach (var order in orders)
             {
@@ -64,7 +127,7 @@ namespace DataToMySQL
             }
         }
 
-        private void InsertProductsIntoDb(IDbSet<Product> products, MySqlConnection connection)
+        private static void InsertProductsIntoDb(IDbSet<Product> products, MySqlConnection connection)
         {
             foreach (var product in products)
             {
@@ -80,7 +143,7 @@ namespace DataToMySQL
             }
         }
 
-        private void InsertSuppliersIntoDb(IDbSet<Supplier> suppliers, MySqlConnection connection)
+        private static void InsertSuppliersIntoDb(IDbSet<Supplier> suppliers, MySqlConnection connection)
         {
             foreach (var supplier in suppliers)
             {
@@ -93,7 +156,7 @@ namespace DataToMySQL
             }
         }
 
-        private void InsertCustomersIntoDb(IDbSet<Customer> customers, MySqlConnection connection)
+        private static void InsertCustomersIntoDb(IDbSet<Customer> customers, MySqlConnection connection)
         {
             foreach (var customer in customers)
             {
@@ -106,7 +169,7 @@ namespace DataToMySQL
             }
         }
 
-        private void InsertCategoriesIntoDb(IDbSet<Category> categories, MySqlConnection connection)
+        private static void InsertCategoriesIntoDb(IDbSet<Category> categories, MySqlConnection connection)
         {
             foreach (var category in categories)
             {
@@ -118,87 +181,16 @@ namespace DataToMySQL
             }
         }
 
-        private void InsertMeasuresIntoDb(IDbSet<Measure> measures, MySqlConnection connection)
+        private static void InsertMeasuresIntoDb(IDbSet<Measure> measures, MySqlConnection connection)
         {
             foreach (var measure in measures)
             {
                 var insertQuery = "insert into measures (name) values (@name)";
                 var cmd = new MySqlCommand(insertQuery, connection);
+                cmd.Prepare();
                 cmd.Parameters.AddWithValue("@name", measure.Name);
                 cmd.ExecuteNonQuery();
             }
         }
-
-
-        private static void InsertOrdersIntoDb(DbSet<Order> orders, MySqlConnection connection)
-        {
-            foreach (var order in orders)
-            {
-                var insertQuery =
-                    "insert into orders (quantity, product_id, discount, customer_id, order_date) values (@quantity, @product_id, @discount, @customer_id, @order_date)";
-                var cmd = new MySqlCommand(insertQuery, connection);
-                cmd.Parameters.AddWithValue("@quantity", order.Quantity);
-                cmd.Parameters.AddWithValue("@product_id", order.ProductId);
-                cmd.Parameters.AddWithValue("@discount", order.Discount);
-                cmd.Parameters.AddWithValue("@customer_id", order.CustomerId);
-                cmd.Parameters.AddWithValue("@order_date", order.Date);
-                cmd.ExecuteNonQuery();
-            }
-        }
-
-        private static void InsertProductsIntoDb(DbSet<Product> products, MySqlConnection connection)
-        {
-            foreach (var product in products)
-            {
-                var insertQuery =
-                    "insert into products (name, price, supplier_id, category_id, measure_id) values (@name, @price, @supplier_id, @category_id, @measure_id)";
-                var cmd = new MySqlCommand(insertQuery, connection);
-                cmd.Parameters.AddWithValue("@name", product.Name);
-                cmd.Parameters.AddWithValue("@price", product.Price);
-                cmd.Parameters.AddWithValue("@supplier_id", product.SupplierId);
-                cmd.Parameters.AddWithValue("@category_id", product.CategoryId);
-                cmd.Parameters.AddWithValue("@measure_id", product.MeasureId);
-                cmd.ExecuteNonQuery();
-            }
-        }
-
-        private static void InsertSuppliersIntoDb(DbSet<Supplier> suppliers, MySqlConnection connection)
-        {
-            foreach (var supplier in suppliers)
-            {
-                var insertQuery = "insert into suppliers (name, address, phone) values (@name, @address, @phone)";
-                var cmd = new MySqlCommand(insertQuery, connection);
-                cmd.Parameters.AddWithValue("@name", supplier.Name);
-                cmd.Parameters.AddWithValue("@address", supplier.Address);
-                cmd.Parameters.AddWithValue("@phone", supplier.Phone);
-                cmd.ExecuteNonQuery();
-            }
-        }
-
-        private static void InsertCustomersIntoDb(DbSet<Customer> customers, MySqlConnection connection)
-        {
-            foreach (var customer in customers)
-            {
-                var insertQuery = "insert into customers (name, address, phone) values (@name, @address, @phone)";
-                var cmd = new MySqlCommand(insertQuery, connection);
-                cmd.Parameters.AddWithValue("@name", customer.Name);
-                cmd.Parameters.AddWithValue("@address", customer.Address);
-                cmd.Parameters.AddWithValue("@phone", customer.Phone);
-                cmd.ExecuteNonQuery();
-            }
-        }
-
-        private static void InsertCategoriesIntoDb(DbSet<Category> categories, MySqlConnection connection)
-        {
-            foreach (var category in categories)
-            {
-                var insertQuery = "insert into categories (name, description) values (@name, @description)";
-                var cmd = new MySqlCommand(insertQuery, connection);
-                cmd.Parameters.AddWithValue("@name", category.Name);
-                cmd.Parameters.AddWithValue("@supermarket", category.Description);
-                cmd.ExecuteNonQuery();
-            }
-        }
-
     }
 }
